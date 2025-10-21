f5b5bd957ba2d0776330efbc91b8df5fd7a6e7d1
optjjj <redtj1@outlook.com>
2025-10-21 11:44:50 -0700

    "lf11_on has manual input code"

--- RaspPi_Light_Controller/lf11_on.py
+++ RaspPi_Light_Controller/lf11_on.py
@@ -1,29 +1,26 @@
-#!/usr/bin/env python3
-import telnetlib, time, sys, os
 
-HOST = os.environ.get("LF11_HOST", "192.168.1.123")
-PORT = int(os.environ.get("LF11_PORT", "50123"))
-LEVEL = int(os.environ.get("LF11_LEVEL_ON", "1000"))  # 0..2000
+import RPi.GPIO as GPIO
+import serial
+EN_485 =  4
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(EN_485,GPIO.OUT)
+GPIO.output(EN_485,GPIO.HIGH)
 
-def send(tn, cmd):
-    tn.write((cmd + "\r\n").encode("ascii"))
-    time.sleep(0.1)
-    _ = tn.read_very_eager()  # ignore output
+t = serial.Serial("/dev/ttyS0",9600, timeout=5)
 
-def main():
-    if not (0 <= LEVEL <= 2000):
-        print("LEVEL must be 0..2000", file=sys.stderr)
-        sys.exit(2)
-    try:
-        with telnetlib.Telnet(HOST, PORT, timeout=5) as tn:
-            send(tn, "?")
-            send(tn, "ecr! 1")
-            send(tn, "mode m")
-            send(tn, f"lo {LEVEL}")
-            send(tn, "ls all")
-    except Exception as e:
-        print(f"error: {e}", file=sys.stderr)
-        sys.exit(1)
+def send_signal(input:str):
+        GPIO.output(EN_485,GPIO.HIGH)
+        print (t.portstr)
+        strInput = input + '\r\n'
+        n = t.write(strInput.encode())
+        print (n)
+        GPIO.output(EN_485,GPIO.LOW)
+        str = t.readline()
+        print (str)
+        return str
 
-if __name__ == "__main__":
-    main()
+
+
+while (1):
+        command = input('command:')
+        send_signal(command)
